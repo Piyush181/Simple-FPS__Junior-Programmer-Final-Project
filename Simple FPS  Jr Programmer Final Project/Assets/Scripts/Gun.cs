37b1f93eb17f99d1213e54e84774e6169a215f19
Piyush181 <78870227+Piyush181@users.noreply.github.com>
2021-06-22 21:43:39 +0300

    Created new scripts and scene
    Created gun and health scripts, and added a gun prefab to the scene. Also added skybox

--- Simple FPS  Jr Programmer Final Project/Assets/Scripts/Gun.cs
+++ Simple FPS  Jr Programmer Final Project/Assets/Scripts/Gun.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    protected float damage { get; private set; } //ENCAPSULATION
+    protected float range { get; private set; } //ENCAPSULATION
+    protected float fireRate { get; private set; } //ENCAPSULATION
+    protected float nextTimeToFire { get; private set; } //ENCAPSULATION
+    public ParticleSystem muzzleFlash;
 
-    // Update is called once per frame
+    protected Camera cam;
+
     void Update()
     {
-        
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            muzzleFlash.Play();
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    protected void Shoot() //ABSTRACTION
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+        {
+            Health target = hitInfo.transform.GetComponent<Health>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+    protected void Start()
+    {
+        SetValues();
+        cam = Camera.main;
+    }
+    protected virtual void SetValues() //ABSTRACTION
+    {
+        damage = 10f;
+        range = 100f;
+        fireRate = 15f;
     }
 }
